85812b06c0b7513013fb0db32f9f2b44523953a6
Nick Gyongyosi <lazarus@Lazarus.local>
2019-10-04 15:37:00 -0600

    script for running models and plotting

--- clean_and_engineer.py
+++ clean_and_engineer.py
@@ -6,14 +6,15 @@ from datetime import datetime
 import calendar
 
 #-------Clean the Data--------#
-def drop_cols(df):
+def drop_rows_cols(df):
+    df.drop([1233], inplace=True)
     df.drop(['Candidate Native location',
             'Name(Cand ID)',
             'Unnamed: 23',
             'Unnamed: 24',
             'Unnamed: 25',
             'Unnamed: 26',
-            'Unnamed: 27'], axis=1, inplace=True)
+            'Unnamed: 27'], axis=1, inplace=True)x
 
 def rename_cols(df):
     df.columns = ['date',
@@ -70,16 +71,6 @@ def clean_date_col(date):
     date = date.str.replace('//+', '/')
     return date
 
-# create new column with day of the week (Monday=0 - Sunday=6)
-def make_weekdays_column(df):
-    df['date'] = pd.to_datetime(df['date'])
-    weekdays = []
-    for i in range(0,len(df['date'])):
-        weekdays.append(df['date'][i].weekday())
-    weekdays = pd.Series(weekdays)
-    df['weekdays'] = weekdays
-    return df
-
 # Cleans locations in a pandas series
 def clean_locations(series):
     ext = series.str.extract('(\w+)')
@@ -93,16 +84,60 @@ def clean_location_cols(df):
     df['interview_loc'] = clean_locations(df['interview_loc'])
     return df
 
+#------Engineer the Data------#
+
+# create new column with day of the week (Monday=0 - Sunday=6)
+def make_weekdays_column(df):
+    df['date'] = pd.to_datetime(df['date'])
+    weekdays = []
+    for i in range(0,len(df['date'])):
+        weekdays.append(df['date'][i].weekday())
+    weekdays = pd.Series(weekdays)
+    df['weekdays'] = weekdays
+    return df
+
+# calculate distances between locations
+def get_distance(series1,series2):
+    # Returns the L2 distance (in km) from series1 to series2; 
+    # series1 and series2 are pandas series containing locations 
+    # From https://distancecalculator.globefeed.com/India_Distance_Calculator.asp   
+    loc_list = ['Bangalore', 'Chennai', 'Cochin', 
+                'Delhi', 'Gurgaon', 'Hosur', 
+                'Hyderabad', 'Noida', 'Visakapatinam']
+
+    distances = np.array(
+        [[   0,  290,  354, 1750,  346,  248,   39, 2127,  799],
+         [ 290,    0,  692, 1768, 1742,  268,  515, 1748,  602],
+         [ 354,  692,    0, 2090, 2062,  356,  863, 2675, 1429],
+         [1750, 1768, 2090,    0,   43, 1777, 1266,   46, 1375],
+         [ 346, 1742, 2062,   43,    0, 1750, 1240,   51, 1354],
+         [ 248,  268,  356, 1777, 1750,    0,  608, 1757,  798],
+         [  39,  515,  863, 1266, 1240,  608,    0, 1536,  503],
+         [2127, 1748, 2675,   46,   51, 1757, 1536,    0, 1344],
+         [ 799,  602, 1429, 1375, 1354,  798,  503, 1344,    0]]
+         )
+
+    dist_list = []
+    for loc1, loc2 in zip(series1.values, series2.values):
+        idx1 = loc_list.index(loc1)
+        idx2 = loc_list.index(loc2)
+        dist_list.append(distances[idx1,idx2])
+    return pd.Series(dist_list, index=series1.index) 
+
 if __name__ == "__main__":
 #------Import and Clean Data------#
     df = pd.read_csv('data/Interview.csv')
-    drop_cols(df)
+    drop_rows_cols(df)
     rename_cols(df)
     clean_yes_no_cols(df)
     df['date'] = clean_date_col(df['date'])
     clean_location_cols(df)
+
 #------Feature Engineering------#
     make_weekdays_column(df)
+    df['d_loc2job'] = get_distance(df['location'], df['cand_job_loc'])
+    df['d_loc2int'] = get_distance(df['location'], df['interview_loc'])
+
 #------Export------#
     df_cleaned = df
-    df_cleaned.to_csv()
+    df_cleaned.to_csv('data/df_cleaned.csv')
